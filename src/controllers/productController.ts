c0c8b332dbd4a66135d68abc307137ab2bdaafe6
Chopen <75852177+diouri844@users.noreply.github.com>
2023-04-09 02:58:57 +0000

    Update Product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import Product from "../models/product/productModel";
-
-
+import { IProduct } from "../models/product/product.interface";
+import Options from '../utils/Paginate';
 // create new product :
 
 
-const createProduct = async (req:Request, res:Response) => {
+export const createProduct = async (req:Request, res:Response) => {
     // extract data from body : 
     try{
         const {
@@ -40,3 +40,62 @@ const createProduct = async (req:Request, res:Response) => {
         );
     }
 }
+
+
+// get product is a function to get product by :
+
+/*
+category, 
+price,
+region,
+country, 
+product type
+*/
+
+export const getProductsByQuery = async (req:Request, res:Response) => {
+    // get filtred products 
+    try{
+        const { category, price, region, country, productType, page = 1, limit = 10 } = req.query; 
+        // get all products :
+        const query: Record<string, any> = {};
+        if (category) {
+            query.category  = category;     
+        }
+        if ( price ){
+            const priceRange = (price as string).split('-');
+            query.price = { $gte: priceRange[0], $lte: priceRange[1] };
+        }
+        if ( region ){
+            query.region = region;
+        }
+        if ( country ){
+            //filter by country :
+            query.country = country;
+        }
+        if ( productType ) {
+            query.productType = productType; 
+        } 
+        res.status(200).json({
+            status:"success",
+            products:await Product.paginate(
+                query,
+                Options
+            )
+        });
+    }
+    catch( err ){
+        res.status(500).json(
+            {
+                status:"unknown",
+                message:err.message   
+            }
+        );
+    }
+        
+}
+
+
+
+
+
+
